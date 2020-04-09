e1e7373622331169688bc448d90e6b36eae6a155
Seda Tathevik Hayrapetyan <sedatathevikhayrapetyan@Sedas-MacBook-Pro.local>
2020-04-09 20:43:50 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout';
+
+
+function App() {
+  return (
+    <Layout>
+      <div style={{width: '400px', height: '500px', border: '1px solid black'}}>
+        <h1>hello</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
